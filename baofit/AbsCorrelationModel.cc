59759b2bb0f32c8527f6a857c295c4d98cd63860
David Kirkby <dkirkby@uci.edu>
2012-04-10 17:10:11 -0700

    Add new abstract base class for correlation function models.

---
+++ baofit/AbsCorrelationModel.cc
@@ -0,0 +1,9 @@
+// Created 07-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "baofit/AbsCorrelationModel.h"
+
+namespace local = baofit;
+
+local::AbsCorrelationModel::AbsCorrelationModel() { }
+
+local::AbsCorrelationModel::~AbsCorrelationModel() { }
---
+++ baofit/AbsCorrelationModel.h
@@ -0,0 +1,23 @@
+// Created 07-Apr-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef BAOFIT_ABS_CORRELATION_MODEL
+#define BAOFIT_ABS_CORRELATION_MODEL
+
+namespace baofit {
+	class AbsCorrelationModel {
+	// Represents an abstract parameterized model of a two-point correlation function.
+	public:
+		AbsCorrelationModel();
+		virtual ~AbsCorrelationModel();
+		// Returns the correlation function evaluated in redshift space where (r,mu) is
+		// the pair separation and z is their average redshift. The separation r should
+		// be provided in h/Mpc.
+        virtual double evaluate(double r, double mu, double z) const = 0;
+        // Returns the azimuthally averaged monopole correlation function evaluated at
+        // comoving separation r in h/Mpc, with an average redshift z.
+        virtual double evaluate(double r, double z) const = 0;
+	private:
+	}; // AbsCorrelationModel
+} // baofit
+
+#endif // BAOFIT_ABS_CORRELATION_MODEL
