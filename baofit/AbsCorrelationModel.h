8bd8c086b5a284a126ce988c01e182e463c85422
David Kirkby <dkirkby@uci.edu>
2012-04-10 17:43:30 -0700

    Add parameter vector to pure virtual evaluate methods.

--- baofit/AbsCorrelationModel.h
+++ baofit/AbsCorrelationModel.h
@@ -3,6 +3,8 @@
 #ifndef BAOFIT_ABS_CORRELATION_MODEL
 #define BAOFIT_ABS_CORRELATION_MODEL
 
+#include <vector>
+
 namespace baofit {
 	class AbsCorrelationModel {
 	// Represents an abstract parameterized model of a two-point correlation function.
@@ -12,10 +14,10 @@ namespace baofit {
 		// Returns the correlation function evaluated in redshift space where (r,mu) is
 		// the pair separation and z is their average redshift. The separation r should
 		// be provided in h/Mpc.
-        virtual double evaluate(double r, double mu, double z) const = 0;
+        virtual double evaluate(double r, double mu, double z, std::vector<double> params) const = 0;
         // Returns the azimuthally averaged monopole correlation function evaluated at
         // comoving separation r in h/Mpc, with an average redshift z.
-        virtual double evaluate(double r, double z) const = 0;
+        virtual double evaluate(double r, double z, std::vector<double> params) const = 0;
 	private:
 	}; // AbsCorrelationModel
 } // baofit
