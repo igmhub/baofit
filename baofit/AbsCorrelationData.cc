704cb502058e69225b16ab4d9f1c5a070ff349c1
David Kirkby <dkirkby@uci.edu>
2012-05-24 17:31:25 -0700

    Implement correlation data base class.

--- baofit/AbsCorrelationData.cc
+++ baofit/AbsCorrelationData.cc
@@ -1,9 +1,30 @@
 // Created 24-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "baofit/AbsCorrelationData.h"
+#include "baofit/RuntimeError.h"
+
+#include "likely/BinnedData.h"
 
 namespace local = baofit;
 
-local::AbsCorrelationData::AbsCorrelationData() { }
+local::AbsCorrelationData::AbsCorrelationData(likely::BinnedDataCPtr data)
+: _data(data)
+{
+}
 
 local::AbsCorrelationData::~AbsCorrelationData() { }
+
+void local::AbsCorrelationData::setData(likely::BinnedDataCPtr data) {
+    if(!_data->isCongruent(*data)) {
+        throw RuntimeError("AbsCorrelationData::setData: new and old data are not congruent.");
+    }
+    _data = data;
+}
+
+int local::AbsCorrelationData::getSize() const {
+    return _data->getNBinsWithData();
+}
+
+double local::AbsCorrelationData::getData(int offset) const {
+    return _data->getData(_data->getIndexAtOffset(offset));
+}
--- baofit/AbsCorrelationData.h
+++ baofit/AbsCorrelationData.h
@@ -3,12 +3,32 @@
 #ifndef BAOFIT_ABS_CORRELATION_DATA
 #define BAOFIT_ABS_CORRELATION_DATA
 
+#include "likely/types.h"
+
 namespace baofit {
 	class AbsCorrelationData {
+	// Represents an abstract mapping from binned data to the (r,mu,z) coordinates
+	// used by an AbsCorrelationModel.
 	public:
-		AbsCorrelationData();
+		AbsCorrelationData(likely::BinnedDataCPtr data);
 		virtual ~AbsCorrelationData();
+		// Replaces our binned data. Throws a RuntimeError unless the new data is
+		// congruent with the original data.
+        void setData(likely::BinnedDataCPtr data);
+        // Returns the number of bins with data.
+        int getSize() const;
+        // Returns the data associated with 0 <= offset < getSize().
+        double getData(int offset) const;
+        // Returns the 3D radius in Mpc/h associated with 0 <= offset < getSize().
+        virtual double getRadius(int offset) const = 0;
+        // Returns the cosine of the angle between the separation vector and
+        // the line of sight (aka mu) associated with 0 <= offset < getSize().
+        // Returns zero if the data is azimuthally averaged.
+        virtual double getCosAngle(int offset) const = 0;
+        // Returns the redshift associated with 0 <= offset < getSize().
+        virtual double getRedshift(int offset) const = 0;
 	private:
+        likely::BinnedDataCPtr _data;
 	}; // AbsCorrelationData
 } // baofit
 
