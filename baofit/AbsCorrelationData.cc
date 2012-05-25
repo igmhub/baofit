ae2d2f45b374b7c43ffce1bba2ba3ab4fa4f7ff5
David Kirkby <dkirkby@uci.edu>
2012-05-24 17:02:32 -0700

    Add skeleton correlation data class hierachy.

---
+++ baofit/AbsCorrelationData.cc
@@ -0,0 +1,9 @@
+// Created 24-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "baofit/AbsCorrelationData.h"
+
+namespace local = baofit;
+
+local::AbsCorrelationData::AbsCorrelationData() { }
+
+local::AbsCorrelationData::~AbsCorrelationData() { }
---
+++ baofit/AbsCorrelationData.h
@@ -0,0 +1,15 @@
+// Created 24-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef BAOFIT_ABS_CORRELATION_DATA
+#define BAOFIT_ABS_CORRELATION_DATA
+
+namespace baofit {
+	class AbsCorrelationData {
+	public:
+		AbsCorrelationData();
+		virtual ~AbsCorrelationData();
+	private:
+	}; // AbsCorrelationData
+} // baofit
+
+#endif // BAOFIT_ABS_CORRELATION_DATA
