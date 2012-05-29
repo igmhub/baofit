e7b9bd2d191ae2dcba1d9717ac5945129ee7aac2
David Kirkby <dkirkby@uci.edu>
2012-05-28 17:38:03 -0700

    Add skeleton class to manage a correlation fn fit.

---
+++ baofit/CorrelationFit.cc
@@ -0,0 +1,9 @@
+// Created 28-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "baofit/CorrelationFit.h"
+
+namespace local = baofit;
+
+local::CorrelationFit::CorrelationFit() { }
+
+local::CorrelationFit::~CorrelationFit() { }
---
+++ baofit/CorrelationFit.h
@@ -0,0 +1,15 @@
+// Created 28-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef BAOFIT_CORRELATION_FIT
+#define BAOFIT_CORRELATION_FIT
+
+namespace baofit {
+	class CorrelationFit {
+	public:
+		CorrelationFit();
+		virtual ~CorrelationFit();
+	private:
+	}; // CorrelationFit
+} // baofit
+
+#endif // BAOFIT_CORRELATION_FIT
