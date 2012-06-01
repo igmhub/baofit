3692e9d54a1e644c2c49616d241fd703e630f204
David Kirkby <dkirkby@uci.edu>
2012-05-31 17:11:04 -0700

    Add verbose ctor flag and implement doBootstrapAnalysis in CorrelationAnalyzer.

--- baofit/CorrelationAnalyzer.cc
+++ baofit/CorrelationAnalyzer.cc
@@ -1,13 +1,22 @@
 // Created 31-May-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "baofit/CorrelationAnalyzer.h"
+#include "baofit/RuntimeError.h"
 #include "baofit/AbsCorrelationData.h"
 #include "baofit/CorrelationFitter.h"
 
+#include "likely/CovarianceAccumulator.h"
+#include "likely/WeightedAccumulator.h"
+#include "likely/FunctionMinimum.h"
+#include "likely/CovarianceMatrix.h"
+
+#include "boost/smart_ptr.hpp"
+#include "boost/format.hpp"
+
 namespace local = baofit;
 
-local::CorrelationAnalyzer::CorrelationAnalyzer(int randomSeed)
-: _resampler(randomSeed)
+local::CorrelationAnalyzer::CorrelationAnalyzer(int randomSeed, bool verbose)
+: _resampler(randomSeed), _verbose(verbose)
 { }
 
 local::CorrelationAnalyzer::~CorrelationAnalyzer() { }
@@ -21,5 +30,70 @@ likely::FunctionMinimumPtr local::CorrelationAnalyzer::fitCombined(std::string c
         boost::dynamic_pointer_cast<baofit::AbsCorrelationData>(_resampler.combined());
     combined->finalize();
     CorrelationFitter fitter(combined,_model);
-    return fitter.fit(method);
+    likely::FunctionMinimumPtr fmin = fitter.fit(method);
+    if(_verbose) fmin->printToStream(std::cout);
+    return fmin;
+}
+
+int local::CorrelationAnalyzer::doBootstrapAnalysis(std::string const &method,
+likely::FunctionMinimumPtr fmin, int bootstrapTrials, int bootstrapSize, bool fixCovariance) const {
+    if(getNData() <= 1) {
+        throw RuntimeError("CorrelationAnalyzer::doBootstrapAnalysis: need > 1 observation.");
+    }
+    if(0 == bootstrapSize) bootstrapSize = getNData();
+    baofit::AbsCorrelationDataPtr bsData;
+    // Lookup the values of floating parameters from the combined fit.
+    std::vector<double> baseline = fmin->getParameters(true);
+    // Initialize statistics accumulators.
+    int nstats = baseline.size()+1;
+    boost::scoped_array<likely::WeightedAccumulator> stats(new likely::WeightedAccumulator[nstats]);
+    likely::CovarianceAccumulator accumulator(nstats);
+    int nInvalid(0);
+
+    for(int trial = 0; trial < bootstrapTrials; ++trial) {
+        // Generate the next boostrap sample.
+        bsData = boost::dynamic_pointer_cast<baofit::AbsCorrelationData>(
+            _resampler.bootstrap(bootstrapSize,fixCovariance));
+        bsData->finalize();
+        // Fit the sample.
+        baofit::CorrelationFitter bsFitEngine(bsData,_model);
+        likely::FunctionMinimumPtr bsMin = bsFitEngine.fit(method);
+        // Accumulate the fit results.
+        if(bsMin->getStatus() == likely::FunctionMinimum::OK) {
+            // Lookup the fitted values of floating parameters.
+            std::vector<double> pvalues = bsMin->getParameters(true);
+            for(int par = 0; par < pvalues.size(); ++par) {
+                // Accumulate statistics for this parameter.
+                stats[par].accumulate(pvalues[par]);
+                // Calculate differences from the baseline fit result (to minimize
+                // roundoff error when accumulating covariance statistics).
+                pvalues[par] -= baseline[par];
+            }
+            // Include the fit chiSquare (relative to the baseline value) in
+            // our statistics.
+            stats[nstats-1].accumulate(bsMin->getMinValue());
+            pvalues.push_back(bsMin->getMinValue() - fmin->getMinValue());
+            accumulator.accumulate(pvalues);
+        }
+        else {
+            nInvalid++;
+        }
+        // Print periodic updates while the analysis is running.
+        if(trial == bootstrapTrials-1 || (_verbose && (0 == (trial+1)%10))) {
+            std::cout << "Completed " << (trial+1) << " bootstrap trials (" << nInvalid
+                << " invalid)" << std::endl;
+        }
+    }
+    // Print a summary of the analysis results.
+    std::vector<std::string> labels(fmin->getNames(true));
+    labels.push_back("ChiSquare");
+    boost::format resultFormat("%20s = %12.6f +/- %12.6f\n");
+    std::cout << std::endl << "Bootstrap Results:" << std::endl;
+    for(int stat = 0; stat < nstats; ++stat) {
+        std::cout << resultFormat % labels[stat] % stats[stat].mean() % stats[stat].error();
+    }
+    std::cout << std::endl << "Bootstrap Errors & Correlations:" << std::endl;
+    accumulator.getCovariance()->printToStream(std::cout,true,"%12.6f",labels);
+    
+    return nInvalid;
 }
--- baofit/CorrelationAnalyzer.h
+++ baofit/CorrelationAnalyzer.h
@@ -10,8 +10,10 @@
 namespace baofit {
 	class CorrelationAnalyzer {
 	public:
-		CorrelationAnalyzer(int randomSeed);
+		CorrelationAnalyzer(int randomSeed, bool verbose = true);
 		virtual ~CorrelationAnalyzer();
+		// Set the verbose level during analysis.
+        void setVerbose(bool value);
 		// Adds a new correlation data object to this analyzer.
         void addData(AbsCorrelationDataCPtr data);
         // Returns the number of data objects added to this analyzer.
@@ -21,11 +23,17 @@ namespace baofit {
         // Fits the combined correlation data aadded to this analyzer and returns
         // the estimated function minimum.
         likely::FunctionMinimumPtr fitCombined(std::string const &method) const;
+        // Performs a bootstrap analysis...
+        // Returns the number of fits to bootstrap samples that failed.
+        int doBootstrapAnalysis(std::string const &method,likely::FunctionMinimumPtr fmin,
+            int bootstrapTrials, int bootstrapSize = 0, bool fixCovariance = true) const;
 	private:
+        bool _verbose;
         likely::BinnedDataResampler _resampler;
         AbsCorrelationModelCPtr _model;
 	}; // CorrelationAnalyzer
 	
+    inline void CorrelationAnalyzer::setVerbose(bool value) { _verbose = value; }
     inline int CorrelationAnalyzer::getNData() const { return _resampler.getNObservations(); }
     inline void CorrelationAnalyzer::setModel(AbsCorrelationModelCPtr model) { _model = model; }
 
