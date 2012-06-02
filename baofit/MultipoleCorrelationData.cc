d5c32c2cf105ac5876da4721a0cb0be1c2d3b277
David Kirkby <dkirkby@uci.edu>
2012-06-02 12:27:33 -0700

    Flesh out MultipoleCorrelationData.

--- baofit/MultipoleCorrelationData.cc
+++ baofit/MultipoleCorrelationData.cc
@@ -1,9 +1,53 @@
 // Created 02-Jun-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "baofit/MultipoleCorrelationData.h"
+#include "baofit/RuntimeError.h"
+
+#include <cmath>
 
 namespace local = baofit;
 
-local::MultipoleCorrelationData::MultipoleCorrelationData() { }
+local::MultipoleCorrelationData::MultipoleCorrelationData(likely::AbsBinningCPtr axis1,
+likely::AbsBinningCPtr axis2, likely::AbsBinningCPtr axis3)
+: AbsCorrelationData(axis1,axis2,axis3,Multipole)
+{ }
+
+local::MultipoleCorrelationData::MultipoleCorrelationData(std::vector<likely::AbsBinningCPtr> axes)
+: AbsCorrelationData(axes,Multipole)
+{
+    if(axes.size() != 3) {
+        throw RuntimeError("MultipoleCorrelationData: expected 3 axes.");
+    }
+}
 
 local::MultipoleCorrelationData::~MultipoleCorrelationData() { }
+
+local::MultipoleCorrelationData *local::MultipoleCorrelationData::clone(bool binningOnly) const {
+    return binningOnly ?
+        new MultipoleCorrelationData(getAxisBinning()) :
+        new MultipoleCorrelationData(*this);
+}
+
+double local::MultipoleCorrelationData::getRadius(int index) const {
+    _setIndex(index);
+    return _rLast;
+}
+
+double local::MultipoleCorrelationData::getRedshift(int index) const {
+    _setIndex(index);
+    return _ellLast;
+}
+
+int local::MultipoleCorrelationData::getMultipole(int index) const {
+    _setIndex(index);
+    return _zLast;
+}
+
+void local::MultipoleCorrelationData::_setIndex(int index) const {
+    if(index == _lastIndex) return;
+    getBinCenters(index,_binCenter);
+    _rLast = _binCenter[0];
+    _ellLast = (int)std::floor(_binCenter[1]+0.5);
+    _zLast = _binCenter[2];
+    _lastIndex = index;
+}
--- baofit/MultipoleCorrelationData.h
+++ baofit/MultipoleCorrelationData.h
@@ -3,12 +3,31 @@
 #ifndef BAOFIT_MULTIPOLE_CORRELATION_DATA
 #define BAOFIT_MULTIPOLE_CORRELATION_DATA
 
+#include "baofit/AbsCorrelationData.h"
+
 namespace baofit {
-	class MultipoleCorrelationData {
+	class MultipoleCorrelationData : public AbsCorrelationData {
+	// Represents a 3D correlation function with its transverse degree of freedom projected
+	// into multipole space.
 	public:
-		MultipoleCorrelationData();
+		MultipoleCorrelationData(likely::AbsBinningCPtr axis1, likely::AbsBinningCPtr axis2,
+		    likely::AbsBinningCPtr axis3);
+        MultipoleCorrelationData(std::vector<likely::AbsBinningCPtr> axes);
 		virtual ~MultipoleCorrelationData();
+		// Polymorphic shallow copy so this type of data can be used with likely::BinnedDataResampler.
+        virtual MultipoleCorrelationData *clone(bool binningOnly = false) const;
+        // Returns the 3D radius in Mpc/h associated with the specified global index.
+        virtual double getRadius(int index) const;
+        // Returns the multipole (0,2,4) associated with the specified global index.
+        virtual int getMultipole(int index) const;
+        // Returns the redshift associated with the specified global index.
+        virtual double getRedshift(int index) const;
 	private:
+        void _setIndex(int index) const;
+        mutable int _lastIndex;
+        mutable std::vector<double> _binCenter;
+        mutable double _rLast, _zLast;
+        mutable int _ellLast;
 	}; // MultipoleCorrelationData
 } // baofit
 
