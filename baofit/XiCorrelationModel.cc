bac58cc2568802d65a564cba9ed0ea5ce3cd4760
David Kirkby <dkirkby@uci.edu>
2012-06-06 17:27:56 -0700

    Flesh out new XiCorrelationModel.

--- baofit/XiCorrelationModel.cc
+++ baofit/XiCorrelationModel.cc
@@ -2,8 +2,40 @@
 
 #include "baofit/XiCorrelationModel.h"
 
+#include "likely/AbsBinning.h"
+
+#include "boost/format.hpp"
+
 namespace local = baofit;
 
-local::XiCorrelationModel::XiCorrelationModel() { }
+local::XiCorrelationModel::XiCorrelationModel(likely::AbsBinningCPtr rbins)
+: AbsCorrelationModel("Xi"), _rbins(rbins)
+{
+    // Create parameters at the center of each radial bin.
+    boost::format pname("xi%d-%d");
+    for(int ell = 0; ell <= 4; ell += 2) {
+        double perror = 1e-6;
+        if(2 == ell) perror = 1e-7;
+        else if(4 == ell) perror = 1e-8;
+        for(int index = 0; index < _rbins->getNBins(); ++index) {
+            double rval(_rbins->getBinCenter(index));
+            defineParameter(boost::str(pname % ell % index),0,1e-6);
+        }
+    }
+}
 
 local::XiCorrelationModel::~XiCorrelationModel() { }
+
+double local::XiCorrelationModel::evaluate(double r, double mu, double z,
+std::vector<double> const &params) const {
+    
+    return 0;
+}
+
+double local::XiCorrelationModel::evaluate(double r, cosmo::Multipole multipole, double z,
+std::vector<double> const &params) const {
+    int index = _rbins->getBinIndex(r);
+    if(multipole == cosmo::Quadrupole) index += _rbins->getNBins();
+    else if(multipole == cosmo::Hexadecapole) index += 2*_rbins->getNBins();
+    return params[index];
+}
--- baofit/XiCorrelationModel.h
+++ baofit/XiCorrelationModel.h
@@ -3,12 +3,26 @@
 #ifndef BAOFIT_XI_CORRELATION_MODEL
 #define BAOFIT_XI_CORRELATION_MODEL
 
+#include "baofit/AbsCorrelationModel.h"
+
+#include "likely/types.h"
+
 namespace baofit {
-	class XiCorrelationModel {
+	class XiCorrelationModel : public AbsCorrelationModel {
 	public:
-		XiCorrelationModel();
+		XiCorrelationModel(likely::AbsBinningCPtr rbins);
 		virtual ~XiCorrelationModel();
+		// Returns the correlation function evaluated in redshift space where (r,mu) is
+		// the pair separation and z is their average redshift. The separation r should
+		// be provided in Mpc/h.
+        virtual double evaluate(double r, double mu, double z,
+            std::vector<double> const &params) const;
+        // Returns the correlation function for the specified multipole at co-moving pair separation
+        // r and average pair redshift z.
+        virtual double evaluate(double r, cosmo::Multipole multipole, double z,
+            std::vector<double> const &params) const;
 	private:
+        likely::AbsBinningCPtr _rbins;
 	}; // XiCorrelationModel
 } // baofit
 
