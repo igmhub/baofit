9beac8ba908161beecb7811019f72cb898a9edde
David Kirkby <dkirkby@uci.edu>
2012-08-31 12:24:45 -0700

    Add skeleton new class PkCorrelationModel

---
+++ baofit/PkCorrelationModel.cc
@@ -0,0 +1,9 @@
+// Created 31-Aug-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "baofit/PkCorrelationModel.h"
+
+namespace local = baofit;
+
+local::PkCorrelationModel::PkCorrelationModel() { }
+
+local::PkCorrelationModel::~PkCorrelationModel() { }
---
+++ baofit/PkCorrelationModel.h
@@ -0,0 +1,15 @@
+// Created 31-Aug-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef BAOFIT_PK_CORRELATION_MODEL
+#define BAOFIT_PK_CORRELATION_MODEL
+
+namespace baofit {
+	class PkCorrelationModel {
+	public:
+		PkCorrelationModel();
+		virtual ~PkCorrelationModel();
+	private:
+	}; // PkCorrelationModel
+} // baofit
+
+#endif // BAOFIT_PK_CORRELATION_MODEL
