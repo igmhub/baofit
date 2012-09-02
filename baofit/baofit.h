e6d94ccc1dfd10e27fde7c7eba4edf0f989f715f
David Kirkby <dkirkby@uci.edu>
2012-09-02 11:48:50 -0700

    Add new class to pkg header.

--- baofit/baofit.h
+++ baofit/baofit.h
@@ -6,6 +6,7 @@
 
 #include "baofit/AbsCorrelationModel.h"
 #include "baofit/BaoCorrelationModel.h"
+#include "baofit/PkCorrelationModel.h"
 #include "baofit/XiCorrelationModel.h"
 
 #include "baofit/AbsCorrelationData.h"
