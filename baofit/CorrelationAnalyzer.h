96787d2bbe472c0872c574d4b6a7034df2d9ff6e
David Kirkby <dkirkby@uci.edu>
2012-09-21 07:51:31 -0700

    Improve comments.

--- baofit/CorrelationAnalyzer.h
+++ baofit/CorrelationAnalyzer.h
@@ -108,8 +108,9 @@ namespace baofit {
         // floating parameters of the specified function minimum. Uses the specified zref to
         // calculate the redshift evolution of the scale and its error.
         bool printScaleZEff(likely::FunctionMinimumCPtr fmin, double zref, std::string const &scaleName) const;
-        // Returns a bootstrap estimate of the combined data's covariance matrix using the specified number
-        // of bootstrap trials.
+        // Returns a bootstrap estimate of the combined data's covariance matrix (before any final cuts)
+        // using the specified number of bootstrap trials. Throws a RuntimeError if the estimated covariance
+        // is not positive definite, which can usually be fixed with more samples.
         likely::CovarianceMatrixPtr estimateCombinedCovariance(int nSamples) const;
 	private:
         std::string _method;
