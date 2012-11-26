fb7ed4362cc721637f0dc425aeead2d6b5d1a40b
David Kirkby <dkirkby@uci.edu>
2012-11-26 10:37:00 -0800

    Add skeleton new class BroadbandModel

---
+++ baofit/BroadbandModel.cc
@@ -0,0 +1,9 @@
+// Created 26-Nov-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "baofit/BroadbandModel.h"
+
+namespace local = baofit;
+
+local::BroadbandModel::BroadbandModel() { }
+
+local::BroadbandModel::~BroadbandModel() { }
---
+++ baofit/BroadbandModel.h
@@ -0,0 +1,15 @@
+// Created 26-Nov-2012 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef BAOFIT_BROADBAND_MODEL
+#define BAOFIT_BROADBAND_MODEL
+
+namespace baofit {
+	class BroadbandModel {
+	public:
+		BroadbandModel();
+		virtual ~BroadbandModel();
+	private:
+	}; // BroadbandModel
+} // baofit
+
+#endif // BAOFIT_BROADBAND_MODEL
